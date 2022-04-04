a045dd8258e893dc72ce971dfa50379099557a38
Eduardo <edumacandj@gmail.com>
2022-04-04 17:35:39 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 550,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
